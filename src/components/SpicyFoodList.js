d7f3f7d9d09057db6fe46c7ac601bd8562f13a9b
Moses <mochengo29@gmail.com>
2023-07-20 13:43:13 +0300

    Completed assignment

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -6,12 +6,20 @@ function SpicyFoodList() {
 
   function handleAddFood() {
     const newFood = getNewRandomSpicyFood();
-    console.log(newFood);
+    // console.log(newFood);
+    let newFoodArray = [...foods, newFood];
+    setFoods(newFoodArray);
+  }
+
+  function handleDeleteFood(id) {
+    const updatedFoodsList = foods.filter((food) => food.id !== id);
+    setFoods(updatedFoodsList);
   }
 
   const foodList = foods.map((food) => (
     <li key={food.id}>
       {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
+      <button onClick={() => handleDeleteFood(food.id)}>Delete a Food</button>
     </li>
   ));
 
